407300af005535f58d5b96de8c156aaa92344f23
aLiruihao <1262946752@qq.com>
2020-02-07 13:59:11 +0800

    SSM框架配置success

---
+++ backend/src/main/java/com/lrh/AuthorityControl/handler/UserHandler.java
@@ -0,0 +1,29 @@
+package com.lrh.AuthorityControl.handler;
+
+        import com.lrh.AuthorityControl.entity.User;
+        import com.lrh.AuthorityControl.service.api.UserService;
+        import org.springframework.beans.factory.annotation.Autowired;
+        import org.springframework.stereotype.Controller;
+        import org.springframework.ui.Model;
+        import org.springframework.web.bind.annotation.RequestMapping;
+
+        import java.util.List;
+
+/**
+ * @author lrhya
+ * @version 1.0
+ * @date 2020/2/7 13:45
+ */
+@Controller
+public class UserHandler {
+
+    @Autowired
+    private UserService userService;
+
+    @RequestMapping("/user/get/all")
+    public String getAll(Model model) {
+        List<User> list = userService.getAll();
+        model.addAttribute("list", list);
+        return "test-target";
+    }
+}
---
+++ front/src/main/webapp/WEB-INF/test-target.jsp
@@ -0,0 +1,23 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Chris
+  Date: 2020/2/7
+  Time: 13:57
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+<c:if test="${!empty list}">
+    <c:forEach  items="${list}" var="user">
+        ${user}<br/>
+    </c:forEach>
+
+</c:if>
+
+
+</body>
+</html>
