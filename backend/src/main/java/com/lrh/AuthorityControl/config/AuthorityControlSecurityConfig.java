3570b50ebc4b80e932650f73b5222c7c0d5d44b8
aLiruihao <1262946752@qq.com>
2020-04-06 16:44:04 +0800

    放行静态资源，内存版登陆成功

--- backend/src/main/java/com/lrh/AuthorityControl/config/AuthorityControlSecurityConfig.java
+++ backend/src/main/java/com/lrh/AuthorityControl/config/AuthorityControlSecurityConfig.java
@@ -1,7 +1,9 @@
 package com.lrh.AuthorityControl.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -15,4 +17,43 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 //表示启用全局方法权限管理功能。
 @EnableGlobalMethodSecurity(prePostEnabled=true)
 public class AuthorityControlSecurityConfig extends WebSecurityConfigurerAdapter {
+
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder builder) throws Exception {
+
+        //	测试使用
+		builder
+			.inMemoryAuthentication()
+			.withUser("xixi")
+			.password("12323")
+			.roles("king");
+      /*  builder.userDetailsService(userDetailsService);
+    /*    builder.userDetailsService(userDetailsService).passwordEncoder(getPasswordEncoder());*/
+    }
+    @Override
+    protected void configure(HttpSecurity security) throws Exception {
+        //放行首页、静态资源。
+        security.authorizeRequests()
+                .antMatchers("/index.html","/statics/bootstrap/**","/statics/css/**","/fonts/**","/statics/images/**","/statics/jquery/**","/statics/layer/**","/statics/script/**","/  statics//ztree/**")
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/index.html")
+                .permitAll()
+                .loginProcessingUrl("/user/do/login")
+                .permitAll()
+                .usernameParameter("loginAcct")
+                .passwordParameter("userPswd")
+                .defaultSuccessUrl("/main/to/page")
+                .and()
+                .csrf()
+                .disable();
+
+
+
+
+    }
 }
